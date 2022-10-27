de569707f14abda04b9ad8f8a574dfb36be3332c
Мурад <limatov.murad@yandex.ru>
2022-10-27 19:31:28 +0300

    больше деталей

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+let countObject = 0;
+
+const getNumberArray = () => {
+  countObject++;
+  return {
+    id: countObject,
+    url: photos/`${countObject}`.jpg,
+    description: "Фотка на память",
+    comments: getRandomPositiveInteger(0, 200),
+    likes: getRandomPositiveInteger(15, 200)
+  };
 };
 
+const ObjectsArray = Array.from({ length: 25 }, getNumberArray);
+
+function getRandomPositiveInteger(a, b) {
+  if (a < 0 || b < 0) {
+    return NaN;
+  }
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+}
+
 const checkStringLength = (string, length) => string.length <= length;
-getRandomPositiveInteger();
-checkStringLength();
+
+checkStringLength('', 140);
